bf06922b89cef07de5eea0c6e8f7a43e9fe99a58
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-29 15:08:13 -0700

    working in areas

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,26 +1,57 @@
 import styled from 'styled-components';
-import { Btnsave } from '..';
-import { useAuthStore } from '../../store/AuthStore';
+
 
 export function HomeTemplate(){
-    const { signOut } = useAuthStore();
     
 
     return(
     <Container>
-        <h1> Home Template </h1>
-        <Btnsave titulo='Cerrar Sesion' bgcolor='#fff' funcion={signOut}/>
+        <header className='header'>
+
+        </header>
+
+        <section className='area1'>
+
+        </section>
+
+        <section className='area2'>
+
+        </section>
+
+        <section className='main'>
+
+        </section>
     </Container>)
 };
 
 //*STYLES
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    width: 100%;
     height: 100vh;
-    overflow: hidden;
     background-color: ${(props) => props.theme.bgtotal};
     color: ${(props) => props.theme.text};
-    width: 100%;
+    display: grid;
+    padding: 15px;
+    grid-template: 
+    "header" 100px
+    "area1" 100px
+    "area2" 100px
+    "main" auto
+    ;
+    .header{
+        grid-area: header;
+        background-color: rgba(103,93,241,0.14);
+    }
+    .area1{
+        grid-area: area1;
+        background-color: rgba(229,67,26,0.14);
+    }
+    .area2{
+        grid-area: area2;
+        background-color: rgba(77,237,106,0.14);
+    }
+    .main{
+        grid-area: main;
+        background-color: rgba(179,46,241,0.14);
+    }
 `
